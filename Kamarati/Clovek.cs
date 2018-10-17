964a63415c72abe921ce5394ca488cf867d7ea70
Smouyk91 <jurajber@gmail.com>
2018-10-17 17:02:35 +0200

    1. verzia

---
+++ Kamarati/Clovek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamarati
+{
+    class Clovek
+    {
+        public string meno;
+        public int vek;
+        public Clovek kamarat;
+
+        public void Predstavenie()
+        {
+            Console.WriteLine("Ahoj, volam sa {0}, mam {1} rokov a moj kamarat je {2}.", meno, vek, kamarat.meno);
+        }
+    }
+}
---
+++ Kamarati/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamarati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clovek clovek1 = new Clovek();
+            clovek1.meno = "Karel Novák";
+            clovek1.vek = 33;
+            Clovek clovek2 = new Clovek();
+            clovek2.meno = "Josef Nový";
+            clovek2.vek = 27;
+
+            clovek1.kamarat = clovek2;
+            clovek2.kamarat = clovek1;
+
+            clovek1.Predstavenie();
+            clovek2.Predstavenie();
+            Console.ReadKey();
+        }
+    }
+}
